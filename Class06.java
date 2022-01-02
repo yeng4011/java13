5ea3bd9516a91473dc280251c6f1a44b56a285be
yeng4011 <89770222+yeng4011@users.noreply.github.com>
2022-01-03 02:53:08 +0800

    Create Class06.java

---
+++ Class06.java
@@ -0,0 +1,19 @@
+package Class06.java;
+
+ public class Class06
+ {
+ public static void main(String args[])
+ {
+ try
+ {
+ int num=12;
+ int den[]={12,0,3,0,0,4};
+ for(int i=0;i<10;i++)
+ System.out.println("ans="+num/den[i]);
+ }
+ catch(Exception e)
+ {
+ System.out.println("捕捉到例外了");
+ }
+ }
+ } 
